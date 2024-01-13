2b024cf5cb70cbd6a40187baaad3622848e7f555
Tyler Harris <tyler.bigdog@gmail.com>
2024-01-12 19:57:47 -0500

    Project 4 final

--- scripts/index.js
+++ scripts/index.js
@@ -30,16 +30,63 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+/*------- Elements --------*/
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModel = document.querySelector("#profile-edit-model");
-const profileEditClose = document.querySelector("#profile-close-button");
+const profileEditClose = profileEditModel.querySelector(
+  "#profile-close-button"
+);
+const profileTitle = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModel.querySelector(".model__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+const cardListEl = document.querySelector(".card__list");
+/*------- Functions --------*/
+
+function closePopup() {
+  profileEditModel.classList.remove("model_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.alt = cardData.name;
+  cardImageEl.src = cardData.link;
+
+  return cardElement;
+}
+
+/*------- Event Handlers --------*/
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/*------- Event Listeners --------*/
 
 profileEditBtn.addEventListener("click", function () {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModel.classList.add("model_opened");
 });
 
-profileEditClose.addEventListener("click", function () {
-  profileEditModel.classList.remove("model_opened");
+profileEditClose.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
 });
