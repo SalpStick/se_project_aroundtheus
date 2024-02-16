01ba423b6d20c95ba07f338e42e3b2b200cf43b3
Tyler Harris <tyler.bigdog@gmail.com>
2024-02-16 11:32:58 -0500

    fixed/ url validation and put validation in its own file

--- scripts/index.js
+++ scripts/index.js
@@ -63,37 +63,6 @@ const cardTemplate =
 
 const cardListEl = document.querySelector(".cards__list");
 
-/*------- Validation -------*/
-
-function enableValidation(modal, firstInput, secondInput) {
-  var submitButton = modal.querySelector(".modal__button");
-
-  //if either either inputs are not filled in yet it breaks the function
-  if (firstInput === "" || secondInput === "") {
-    return;
-  }
-
-  // Check if both inputs are valid
-  if (firstInput.checkValidity() && secondInput.checkValidity()) {
-    submitButton.removeAttribute("disabled", true);
-    submitButton.classList.add("modal__button_enabled");
-  }
-  if (!firstInput.checkValidity()) {
-    submitButton.setAttribute("disabled", true);
-    submitButton.classList.remove("modal__button_enabled");
-    showErrorMessage(modal, firstInput);
-  } else {
-    hideErrorMessage(modal, firstInput);
-  }
-  if (!secondInput.checkValidity()) {
-    submitButton.setAttribute("disabled", true);
-    submitButton.classList.remove("modal__button_enabled");
-    showErrorMessage(modal, secondInput);
-  } else {
-    hideErrorMessage(modal, secondInput);
-  }
-}
-
 /*------- Functions --------*/
 
 function closePopup(modal) {
@@ -114,18 +83,6 @@ function openPopup(modal) {
 
   //makes submit button invalid
   //modal.querySelector(".modal__btn").classList.remove(".modal__button_enabled");
-}
-
-function showErrorMessage(modal, inputEl) {
-  const errorMessageEl = modal.querySelector("#" + inputEl.id + "-error");
-  errorMessageEl.classList.remove("modal__error_disabled");
-  errorMessageEl.textContent = inputEl.validationMessage;
-  console.log("here");
-}
-
-function hideErrorMessage(modal, inputEl) {
-  const errorMessageEl = modal.querySelector("#" + inputEl.id + "-error");
-  errorMessageEl.classList.add("modal__error_disabled");
 }
 
 function getCardElement(cardData) {
