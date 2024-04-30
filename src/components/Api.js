320f9663d47ac9a87aab8da85fe325dedd29b315
SalpStick <43050248+SalpStick@users.noreply.github.com>
2024-04-29 21:26:12 -0400

    feat/ added request function in Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -16,62 +16,69 @@ export default class Api {
     }
   }
 
+  _request(url, options) {
+    console.log(url);
+    console.log(options);
+    console.log(fetch(url, options));
+    return fetch(url, options).then(this.renderResult);
+  }
+
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(this._baseUrl + `/cards`, {
       method: "GET",
       headers: this._headers,
-    }).then(this.renderResult);
+    });
   }
 
   addCard({ name, url }) {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(this._baseUrl + `/cards`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({ name, link: url }),
-    }).then(this.renderResult);
+    });
   }
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(this._baseUrl + `/users/me`, {
       method: "GET",
       headers: this._headers,
-    }).then(this.renderResult);
+    });
   }
 
   updateUserInfo(name, about) {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(this._baseUrl + `/users/me`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({ name: name, about: about }),
-    }).then(this.renderResult);
+    });
   }
 
   updateAvatar(url) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    return this._request(this._baseUrl + `/users/me/avatar`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({ avatar: url.avatar }),
-    }).then(this.renderResult);
+    });
   }
 
   deleteCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+    return this._request(`${this._url}/cards/${cardId}`, {
       method: "DELETE",
       headers: this._headers,
-    }).then(this.renderResult);
+    });
   }
 
   likeCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+    return this._request(`${this._url}/cards/${cardId}/likes`, {
       method: "PUT",
       headers: this._headers,
-    }).then(this.renderResult);
+    });
   }
 
   dislikeCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+    return this._request(`${this._url}/cards/${cardId}/likes`, {
       method: "DELETE",
       headers: this._headers,
-    }).then(this.renderResult);
+    });
   }
 }
